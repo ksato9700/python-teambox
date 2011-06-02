227b7dd21f765091f19dbac7d322f70f13c1add8
Ken Sato <ksato9700@gmail.com>
2011-06-02 02:54:17 -0700

    forgot to commit the modified file

--- client.py
+++ client.py
@@ -6,45 +6,38 @@ import urllib2
 import json
 import base64
 
+from model import TeamboxObject
+
 URL_PREFIX = "https://teambox.com/api/1/"
 debug = 100
 
-class TeamboxError(Exception):
-    def __init__(self, errors):
-        super(TeamboxError, self).__init__(
-            "TeamboxError: %s: %s" % (errors['type'], errors['message']))
-        
-
 class TeamboxClient(object):
     def __init__(self, credential=None):
-        self.headers = {}
+        self.headers = {
+            'Accept': 'application/json',
+            }
         if credential:
             self.headers['Authorization'] = "Basic %s" % base64.b64encode(credential).strip()
 
         self.openr = urllib2.build_opener(urllib2.HTTPCookieProcessor(),
                                           urllib2.HTTPSHandler(debug),
                                           )
-        self.sf = None
 
-    def _make_request(self, command, params=None):
+    def _make_request(self, command, params=None, headers={}):
         if params:
             data = urllib.urlencode(params)
         else:
             data = None
 
-        headers = {}
         headers.update(self.headers)
         try:
             url = URL_PREFIX + command
-            print url
+            #print url
             f = self.openr.open(urllib2.Request(url=url,
                                                 data=data,
                                                 headers=headers))
-            response = json.load(f)
-            if 'errors' in response:
-                raise TeamboxError(response['errors'])
-            if self.sf:
-                self.sf.write(json.dumps(response))
+
+            response = TeamboxObject.generate_obj(json.load(f))
             return response
             
         except urllib2.HTTPError as e:
@@ -60,8 +53,11 @@ def main():
     import sys
 
     client = TeamboxClient(sys.argv[1])
+
+    #TeamboxObject.sf = open("jsr.json", "w")
     response = client.projects()
-    print json.dumps(response, indent=4)
+    print response
+    #TeamboxObject.sf.close()
 
 if __name__ == "__main__":
     main()
